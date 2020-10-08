d505e1ae152aaeef7ec4ecf1b35b9babd0d3e626
yxie21 <71443347+yxie21@users.noreply.github.com>
2020-10-08 13:35:11 +0800

    Update DropFolder.java

--- DropFolder.java
+++ DropFolder.java
@@ -1,37 +1,58 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
 
-public class DropFolder {
+public class MyFolder {
 
-	public static void main(String[] args) throws IOException {
-		// read in the path of the input folder 
-		
-		String folderPath = "/Users/yuxinxie/Documents/folder";
+	public static void main(String args[]) throws IOException {
+		String workingDir = System.getProperty("user.dir");
+		String configPath = workingDir + File.separator + "config.txt"; // using relative path
+		BufferedReader config = new BufferedReader(new FileReader(configPath));
+
+		Map<String, String> map = new HashMap<>();
+
+
+		while(true) {
+            String entry = config.readLine();
+            if(entry == null || entry.trim().equals("")) {
+            	break;
+            }
+		StringTokenizer st = new StringTokenizer(entry, "=");
+		map.put(st.nextToken(), st.nextToken());
+
+		}
+		config.close();
+		String folderPath = map.get("rootpath");
+		String suffix = map.get("suffix");
 		
 		File[] list = new File(folderPath).listFiles();
 
 		for (File folders : list) {
-			
 			if (folders.isDirectory()) {
-				
 				int folderCount = 0;
-				
 				for (File subfolder : folders.listFiles()) {
 					if (subfolder.isDirectory()) {
 						folderCount++;
 						for (File source : subfolder.listFiles()) {
-							if(source.getName().endsWith(".txt")) {
-								source.renameTo(new File(source.getParentFile().getParent() + "\\" + source.getName()));
-							}							
+							if (source.getName().endsWith(suffix)) {
+								source.renameTo(new File(source.getParentFile().getParent() + File.separator + source.getName()));
+
+							}
 						}
 					}
 				}
-				
-				if(folderCount == 0) {
-					
-					System.err.println(folders.getName() + "did not create a two-layered folder");
+				if (folderCount == 0) {
+					System.err.println(folders.getName() + "");
 				}
 			}
+
 		}
 
 	}
